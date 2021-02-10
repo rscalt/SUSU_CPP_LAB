5955b3a48727ded7d1b0d97b334cf4610bdead35
rscugh <asgardrowizard@yandex.ru>
2021-02-10 15:46:22 +0500

    1

--- labs/listings/control/practice/test.cpp
+++ labs/listings/control/practice/test.cpp
@@ -1,10 +1,13 @@
-/* //считалка с выбыванием до тех пор, пока не останется только один
+#include <iostream>
+
+//считалка с выбыванием до тех пор, пока не останется только один
 float number_game(float *arr, int arr_length)
 {
+
     int word_count; //слов в считалке; 
     //после выбывающего начинает считать тот, кто следующий в круге
-    cout << "Enter words number:";
-    cin >> word_count;
+    std::cout << "Enter words number:";
+    std::cin >> word_count;
 
     int index_to_out = 0; //кто выбывает
     int index_to_start = 0; //кто начинает считать
@@ -16,23 +19,24 @@ float number_game(float *arr, int arr_length)
     while (arr_length > 1)
     {
         for(int j = 0; j < full_length; j++ ) 
-            cout << arr[j] << " ";
-        cout << "\t";
+            std::cout << arr[j] << " ";
+        std::cout << "\t";
 
-        cout << "i: " << i << "  ";
-        cout << "arr_length: " << arr_length << "  ";
+        std::cout << "i: " << i << "  ";
+        std::cout << "arr_length: " << arr_length << "  ";
 
         last_index = arr_length-1;
         length_left  = (last_index - index_to_start)+1;// для (1 - 0) length_left = 2, как в хроестоматийной задаче про забор
 
-        cout << "index_to_start: " << index_to_start << "  ";
-        cout << "length_left: " << length_left << "  ";
+        std::cout << "index_to_start: " << index_to_start << "  ";
+        std::cout << "length_left: " << length_left << "  ";
+
+        index_to_out = word_count%(length_left); //индекс того, кто выбывает;       периодичность (движение по кругу) процесса реализуется через modf 
         
-        index_to_out = word_count%(length_left); //индекс того, кто выбывает; периодичность (движение по кругу) процесса реализуется через modf 
         if (index_to_out < 0) index_to_out += 1; 
 
-        cout << "index_to_out: " << index_to_out << "  ";
-        cout << "nubm_to_out: " << index_to_out+1 << "  ";
+        std::cout << "index_to_out: " << index_to_out << "  ";
+        std::cout << "nubm_to_out: " << index_to_out+1 << "  ";
 
         for (int i = index_to_out; i < last_index; i++) //смещаем элементы влево
             arr[i] = arr[i+1]; //текущий элемент = следующему
@@ -41,9 +45,9 @@ float number_game(float *arr, int arr_length)
 
         index_to_start = index_to_out;
 
-        cout << "index_to_start (new): " << index_to_start << "\t";
+        std::cout << "index_to_start (new): " << index_to_start << "\t";
         i++;
-        cout << endl;
+        std::cout << "\n";
     }
     return arr[0];
-} */
+}
